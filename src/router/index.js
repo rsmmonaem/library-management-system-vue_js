461611fb8ecfa76cb3dbe15b5787debbe2704ff3
MOHAMMAD MUNAYAM SOWDAGOR <72827332+rsmmonaem@users.noreply.github.com>
2024-05-21 12:03:11 +0600

    up

--- src/router/index.js
+++ src/router/index.js
@@ -23,9 +23,13 @@ import NotFound from '../Components/NotFound.vue' // Import the NotFound compone
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-
     {
       path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
       name: 'login',
       component: Login
     },
