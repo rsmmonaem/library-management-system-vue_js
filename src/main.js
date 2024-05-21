f4c9561d050f43bef546f0ff82286e15ec2c6633
MOHAMMAD MUNAYAM SOWDAGOR <72827332+rsmmonaem@users.noreply.github.com>
2024-05-21 21:58:24 +0600

    ed

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
+import './assets/admin/js/scripts.js'
 
 import router from './router'
 const app = createApp(App)
